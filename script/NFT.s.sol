8bd5b2e895674bc2db9e4b140c28a2f1e17ebc21
Renan Mav <renan.mav@hotmail.com>
2024-10-07 18:09:57 -0300

    rename NFT.s.sol

--- script/NFT.s.sol
+++ script/NFT.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "../src/NFT.sol";
 
-contract MintNFTScript is Script {
+contract NFTScript is Script {
     function run() external {
         vm.startBroadcast();
 
