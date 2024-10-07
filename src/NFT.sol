ba1db0d4ffc263deadcaa163563569a9796e48cf
Renan Mav <renan.mav@hotmail.com>
2024-10-07 14:23:35 -0300

    finish NFT.sol code

--- src/NFT.sol
+++ src/NFT.sol
@@ -4,5 +4,13 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
+    uint256 public currentTokenId;
+
     constructor() ERC721("My NFT", "NFT") {}
+
+    function mint(address recipient) public payable returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
 }
